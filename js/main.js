7b6ae68f94132d4f7f0aa371d77437a661e21e2d
elukuent <elukuent@gmail.com>
2016-05-12 14:18:52 -0400

    basic reqs and stretch goals

--- js/main.js
+++ js/main.js
@@ -6,3 +6,67 @@
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+
+$(document).ready(function() {
+  
+  $(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+  });
+
+  jQuery.validator.addMethod("lettersonly", function(value, element) {
+    return this.optional(element) || /^[a-z ]+$/i.test(value);
+  }, "Letters and spaces only please");
+  
+  
+  $("#order-form").validate({
+    rules: {
+
+      "your-name": {
+        maxlength: 128,
+        lettersonly: true,
+      },
+
+      "your-address": "required",
+
+      "your-city" : "required",
+      
+      "your-state" : {
+        required : true,
+        minlength : 2,
+      },
+      
+      "your-zip" : {
+        required : true,
+        minlength : 5,
+        digits : true,
+      },
+      
+      "card-holder-name" : {
+        required : true,
+        equalTo : "#your-name",
+      },
+      
+      "card-number" : {
+        required : true,
+        digits : true,
+        minlength : 16,
+      },
+      
+      "expiry-month" : "required",
+      "expiry-year"  : "required",
+      
+      "cvv" : {
+        required : true,
+        digits : true,
+        minlength : 3,
+      },
+      
+      "shipping-method" : "required",
+      
+      "comments" : {
+        maxlength : 500, 
+      }
+    }
+  });
+
+})
